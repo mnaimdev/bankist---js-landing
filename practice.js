9bb71d5a7130a2f26d1a892faaa7eed0ec73957a
Mohammad Naim Khan <mohammadnaimkhan66@gmail.com>
2023-11-03 18:01:05 +0600

    nav

--- practice.js
+++ practice.js
@@ -39,17 +39,6 @@ document.addEventListener('keydown', (e) => {
 
 // --------------------- cookie message --------------------------- //
 
-// const header = document.querySelector('.header');
-// const message = document.createElement('div');
-// message.classList.add('cookie-message');
-// message.innerHTML = 'We use cookie for analytics & user experience. <button class="btn btn-cookie">Got it</button>';
-// header.append(message);
-
-// document.querySelector('.btn-cookie').addEventListener('click', () => {
-//     message.remove();
-// });
-
-
 const header = document.querySelector('.header');
 const message = document.createElement('div');
 message.classList.add('cookie-message');
@@ -59,4 +48,88 @@ header.append(message);
 
 document.querySelector('.btn-cookie').addEventListener("click", () => {
    message.remove();
+});
+
+
+// --------------------------- end cookie message ---------------------- //
+
+
+// -------------------------- event, bubble, deligate ----------------- //
+
+// event
+// const logo = document.querySelector('.nav__logo');
+// logo.onmouseenter = () => {
+//     alert('mouseenter event call');
+// }
+
+// logo.onmouseover = () => {
+//     alert('mouseover event call');
+// }
+
+// logo.onclick = () => {
+//     alert('onclick event call');
+// }
+
+
+// event bubble
+// const nav = document.querySelector('.nav');
+// const navLinks = document.querySelector('.nav__links');
+// const navLink = document.querySelectorAll('.nav__link');
+
+// const randomInt = (min, max) => {
+//     return Math.floor(Math.random() * (max - min + 1) + 1);
+// }
+
+// const randomColor = () => {
+//     return `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+// }
+
+// nav.addEventListener('click', function() {
+//     this.style.backgroundColor = randomColor();
+// });
+
+// navLinks.addEventListener('click', function() {
+//     this.style.backgroundColor = randomColor();
+// });
+
+
+// navLink.forEach((item) => {
+//     item.addEventListener('click', function() {
+//         this.style.backgroundColor = randomColor();
+//     });
+// })
+
+// event deligate and smooth scroll
+const navLinks = document.querySelector('.nav__links');
+navLinks.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({
+        behavior: 'smooth',
+    });
+});
+
+// learn more section
+document.querySelector('.btn--scroll-to').addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const id = this.getAttribute('href');
+    document.querySelector(id).scrollIntoView({
+        behavior: 'smooth',
+    })
+});
+
+
+// back to top
+const backToTopBtn = document.querySelector('.scroll-top');
+backToTopBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+
+   window.scroll({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+   })
+
 });
